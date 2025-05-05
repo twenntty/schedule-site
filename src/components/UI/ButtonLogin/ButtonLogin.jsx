7090aa87e598316e729661327bf3bc4426a9db1f
Mark Bilous <belousme601@gmail.com>
2025-05-05 22:26:20 +0300

    edit buttonlogin

--- src/components/UI/ButtonLogin/ButtonLogin.jsx
+++ src/components/UI/ButtonLogin/ButtonLogin.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import './ButtonLogin.css'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./ButtonLogin.css";
 
 const ButtonLogin = () => {
-  return (
-    <Link to="/auth">
-    <button className="button_login" >Увійти до Sched</button>
-    </Link>
-  )
-}
+    const navigate = useNavigate();
 
-export default ButtonLogin
+    const handleLoginClick = () => {
+        navigate("/auth");
+    };
+
+    return (
+        <button className="button_login" onClick={handleLoginClick}>
+            Увійти до Sched
+        </button>
+    );
+};
+
+export default ButtonLogin;
